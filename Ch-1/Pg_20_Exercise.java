b5e42ddb65ff594403b060dd80d87f9e876cf0f7
Varun <sharmavarun.1912@gmail.com>
2024-07-07 13:17:30 +0530

    Chapter-1 Complete

---
+++ Ch-1/Pg_20_Exercise.java
@@ -0,0 +1,20 @@
+// Code Magnets
+public class Pg_20_Exercise {
+    public static void main(String[] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");
+            }
+            x = x - 1;
+            System.out.print("-");
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
